1d4aa8f8312a40d3e3031edc37a2e1fc5262ff7e
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2014-08-24 20:51:34 +0200

    First commit

---
+++ ShuckLib/Podcasts.fs
@@ -0,0 +1,6 @@
+﻿(* Copyright (c) 2014, Iwan van der Kleijn (iwanvanderkleijn@gmail.com) - see the file LICENSE for details *) 
+module shuck.Podcasts
+
+type Class1() = 
+    member this.X = "F#"
+
