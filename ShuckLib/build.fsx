c802e8bc74b9f60fdcbe6a5c6c587c26286d6011
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2014-08-24 23:23:06 +0200

    'Build' target added to build.fsx; Now builds from command line

--- ShuckLib/build.fsx
+++ ShuckLib/build.fsx
@@ -2,10 +2,28 @@
 #r @"../packages/FAKE.3.2.17/tools/FakeLib.dll"
 open Fake
 
-// Default target
+// Properties
+let buildDir = "./bin/"
+
+// Targets
+Target "Clean" (fun _ ->
+    CleanDir buildDir
+)
+
+Target "BuildApp" (fun _ ->
+    !! "*.fsproj"
+      |> MSBuildRelease buildDir "Build"
+      |> Log "AppBuild-Output: "
+)
+
 Target "Default" (fun _ ->
     trace "Hello World from FAKE"
 )
+
+// Dependencies
+"Clean"
+  ==> "BuildApp"
+  ==> "Default"
 
 // start build
 RunTargetOrDefault "Default"
