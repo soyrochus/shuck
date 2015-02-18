c032c745cf6bc5db95f032e13f41493a895bac4f
Iwan van der Kleijn <iwanvanderkleijn@gmail.com>
2015-02-18 22:32:40 +0100

    Using Paket instead of Nuget; Upgrade Websharper 3; Include NUnit

---
+++ ShuckLib/Podcasts.fs
@@ -0,0 +1,4 @@
+﻿namespace Schuck
+open System
+
+
---
+++ ShuckLib/Types.fs
@@ -0,0 +1,35 @@
+﻿namespace Schuck
+open System
+
+module Types = 
+
+    type Id = string
+    type Line = string
+    type Text = string
+    type Url = string 
+    type Date = DateTime
+    type EpisodeStatus = 
+        | New
+        | Untouched
+        | Touched
+
+    type PodcastInfo = {
+            Id: Id
+            Name: Line
+            Url: Url
+            Description: Text
+            LastChanged: Date
+    }
+    
+    type Episode = {
+        Id: Id
+        Number: Line
+        Name: Line
+        Url: Url
+        Description: Text 
+        Published: Date
+        Status: EpisodeStatus
+    }
+    type Podcast = PodcastInfo * Episode list 
+    type Podcasts = Podcast list
+
